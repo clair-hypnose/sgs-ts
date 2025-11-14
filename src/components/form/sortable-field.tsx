beb862d3a576ffce641704dcf40614899e26630b
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2025-11-14 19:31:56 +0400

    Update sortable-field.tsx

--- src/components/form/sortable-field.tsx
+++ src/components/form/sortable-field.tsx
@@ -7,13 +7,12 @@ import type { FieldProps } from "./utils";
 // MAIN ------------------------------------------------------------------------------------------------------------------------------------
 export function SortableField<V extends FieldValues, N extends FieldPath<V>>({ legend, ...props }: SortableFieldProps<V, N>) {
   const { field, fieldState } = useController(props);
-  const { invalid } = fieldState;
   const { onChange, value } = field;
   return (
     <FieldSet>
       <FieldLegend>{legend}</FieldLegend>
       <FieldDescription>(Déplacez les réponses du plus important au moins important)</FieldDescription>
-      <Field data-invalid={invalid} orientation="horizontal">
+      <Field orientation="horizontal">
         <SortableList onValueChange={onChange} value={value} />
       </Field>
       <FieldError {...fieldState} />
